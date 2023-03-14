ca0fd2fc0707d8b3dfc50bb54d31c3db930a9135
Lucasteisouza <lucasteisouza@gmail.com>
2023-03-14 17:03:21 -0300

    feat: adiciona validacao ao POST de login

--- src/Routes/loginRoute.js
+++ src/Routes/loginRoute.js
@@ -6,8 +6,18 @@ const router = express.Router();
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({ message: 'Email e senha são obrigatórios' });
+  const regexEmail = /\S+@\S+\.\S+/;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  if (regexEmail.test(email) === false) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   return res.status(200).json({
     token: Math.random().toString(36).substring(2, 10)
